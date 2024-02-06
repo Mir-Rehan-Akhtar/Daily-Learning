a08261dc91954d4b1428ed2d9a338ce1621bfc5c
Mir-Rehan-Akhtar <158301748+Mir-Rehan-Akhtar@users.noreply.github.com>
2024-02-06 17:16:30 +0530

    Add files via upload

---
+++ evenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+
+        if(n%2 == 0 )
+        System.out.println("Even");
+//        if (n%2 == 1)
+//        System.out.println("Odd");
+        else System.out.println("Odd");
+
+
+    }
+}
